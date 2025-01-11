46ae5c05199397cdeb49366a62b4323c49423063
chandan <dbgchandan441@gmail.com>
2025-01-11 14:05:59 +0530

    frontend side

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <>
+      <img style={{ width: "80px"}} src="https://images.seeklogo.com/logo-png/44/1/next-js-logo-png_seeklogo-449824.png?v=1957220810437665024" alt="logo" />
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://www.youtube.com/channel/UCal5EaAQTzvXQk3LXk2FUWw',
+    icon: (
+      <>
+        <img style={{ width: "35px"}} src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png" alt="logo" />
+      </>
+    )
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'MS Docs Template',
   },
 }
 
